7d5e57638f9516319c1f7aeeb9cf2ba41e5118bb
NolionsChen <nolionschen@jetfuel.com.tw>
2019-11-26 14:08:03 +0800

    [revise] 完善simple http server  ＆新增postmain 檔案

--- http_server/main.go
+++ http_server/main.go
@@ -2,96 +2,133 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
+	"os"
 )
 
 type User struct {
-	Name   string `json:"name"`
-	Remark string `json:"remark"`
+	Uid string `json:"uid"`
+	InputUser
+}
+
+type InputUser struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 var users []User
 
-type account struct {
-	UserName string `json:"username"`
-	Password string `json:"password"`
+func newUser() *User {
+	return &User{Uid: uuid.Must(uuid.NewV4()).String()}
 }
 
 func main() {
+	seed()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("GAE_PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
 
 	r := gin.Default()
-	r.GET("/ping", ping)
+	r.GET("/", indexHandler)
+	r.GET("/health", healthHandler)
+	r.GET("/user/:uid", findHandler)
+	r.POST("/user", creatorHandler)
+	r.PUT("/user/:uid", editHandler)
+	r.DELETE("/user/:uid", removeHandler)
+	r.GET("/users", allHandler)
 
-	r.POST("/login", login)
-
-	r.GET("/users", all)
-	r.GET("/user/:id", get)
-	r.POST("/user", creator)
-	r.PUT("/user/:id", edit)
-	r.DELETE("/user/:id", delete)
-
-	err := r.Run(":7777")
-	if err != nil {
-		log.Fatal("error:", err)
-	}
+	log.Printf("Listening on port %s", port)
+	log.Printf("Open http://localhost:%s in the browser", port)
+	log.Fatal(r.Run(fmt.Sprintf(":%s", port)))
 }
 
-func login(c *gin.Context) {
-	fmt.Println(c.Request.Header.Get("Content-Type"))
-	fmt.Println(c.Request.Header.Get("token"))
-
-	a := new(account)
-	err := c.BindJSON(&a)
-	if err != nil {
-		log.Fatal("error:", err)
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"id":    1,
-		"token": "112222",
-	})
+func indexHandler(c *gin.Context) {
+	c.String(http.StatusOK, "Hello, World")
 }
 
-func creator(c *gin.Context) {
-	u := new(User)
-	err := c.BindJSON(&u)
-	if err != nil {
-		log.Fatal("error:", err)
-	}
-
-	users = append(users, *u)
-}
-
-func all(c *gin.Context) {
-	c.JSON(http.StatusOK, users)
-}
-
-func get(c *gin.Context) {
-	id := getUserID(c)
-	fmt.Println(id)
-}
-
-func edit(c *gin.Context) {
-	id := getUserID(c)
-	fmt.Println(id)
-}
-
-func delete(c *gin.Context) {
-	id := getUserID(c)
-	fmt.Println(id)
-}
-
-// heath check
-func ping(c *gin.Context) {
+func healthHandler(c *gin.Context) {
 	c.JSON(http.StatusNoContent, gin.H{})
 }
 
-func getUserID(c *gin.Context) int {
-	id, _ := strconv.Atoi(c.Param("id"))
+func creatorHandler(c *gin.Context) {
+	ru := new(InputUser)
+	err := c.BindJSON(&ru)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
 
-	return id
+	u := newUser()
+	u.Name = ru.Name
+	u.Email = ru.Email
+	users = append(users, *u)
+	c.JSON(http.StatusNoContent, gin.H{})
+}
+
+func editHandler(c *gin.Context) {
+	i := findUserIndex(getUserID(c))
+	u := &users[i]
+
+	ru := new(InputUser)
+	err := c.BindJSON(&ru)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
+	u.Email = ru.Email
+	u.Name = ru.Name
+}
+
+func removeHandler(c *gin.Context) {
+	i := findUserIndex(getUserID(c))
+
+	if i != -1 {
+		users = append(users[:i], users[i+1:]...)
+	}
+
+	c.JSON(http.StatusNoContent, gin.H{})
+}
+
+func allHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, users)
+}
+
+func findHandler(c *gin.Context) {
+	i := findUserIndex(getUserID(c))
+
+	c.JSON(http.StatusOK, users[i])
+}
+
+func seed() {
+	users = append(users, User{
+		Uid: uuid.Must(uuid.NewV4()).String(),
+		InputUser: InputUser{
+		Name:  "John",
+		Email: "John@gmail.com",
+	},
+	})
+}
+
+func getUserID(c *gin.Context) string {
+	return c.Param("uid")
+}
+
+func findUserIndex(uid string) int {
+	for i := 0; i < len(users); i++ {
+		if uid == users[i].Uid {
+			return i
+		}
+	}
+	return -1
 }
